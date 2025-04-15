ce637f381618b2508bf54959a64fd1aa452f8c92
Lebed <tireonreg@gmail.com>
2025-04-15 14:18:54 +0300

    Удалил node_modules из репозитория и добавил в .gitignore

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  app.enableCors(); // Включаем CORS
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
   const port = configService.get<number>('PORT');
-  aw
+  await app.listen(port ?? 5000);
+}
+bootstrap();
